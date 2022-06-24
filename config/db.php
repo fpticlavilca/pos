25769540c7f67252dcc70484fbb2f709217f085e
Paul Sabbath <pticlavilca@gmail.com>
2022-06-24 00:34:26 -0500

    add files

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+  const server = "localhost";
+  const user = "root";
+  const password = "";
+  const database = "pos";
+  const charset = "utf-8";
+  const base_url = "http://localhost/";
+?>
